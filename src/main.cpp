b1ecca391d238fd6fd886b49732024aa61254ef7
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2024-11-06 22:50:41 +0100

    better debuggign

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #include "slang.h"
 
+Slang s(48000);
+
 void setup() {
   Serial.begin(115200);
   pinMode(2, OUTPUT);
   digitalWrite(2, HIGH);
-  Slang s(48000);
-  s.tokenize("sinesynth(220);sinesynth(440);");
-  s.printTokens();
-  //s.interpret();
+  s.tokenize("sinesynth(220); sinesynth(440);");
 }
 
 void loop() {
+  s.interpret();
+  s.printTokens();
+  sleep(3);
 }
