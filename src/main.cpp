3da839de4060733c7918e1636952abf859d9fd4d
gespel <heimbrodt@uni-potsdam.de>
2024-11-13 23:01:27 +0100

    moved tokenizer

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 #include "slang/slang.h"
+#include "slang/tokenizer.h"
 
-Slang s(48000);
+
+Tokenizer t;
+Slang s(48000, t.tokenize("sinesynth(220); sinesynth(440); sinesynth(7000);"));
 
 void setup() {
   Serial.begin(115200);
   pinMode(2, OUTPUT);
   digitalWrite(2, HIGH);
-  s.tokenize("sinesynth(220); sinesynth(440); sinesynth(7000);");
   s.interpret();
 
 }
