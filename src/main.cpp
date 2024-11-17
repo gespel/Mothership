70f3f9e11c7fed40433088280591501425a761f9
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2024-11-17 19:30:59 +0100

    fixed tabsize

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,24 @@ Tokenizer t;
 Slang s(48000, t.tokenize("sinesynth(220); sinesynth(440); sinesynth(7000); sawtoothsynth(200);"));
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
-  s.interpret();
+    Serial.begin(115200);
+    pinMode(2, OUTPUT);
+    digitalWrite(2, HIGH);
+    s.interpret();
 
 }
 
 void loop() {
-  //s.printTokens();
-  for(auto sy : s.getSineSynths()) {
-    //Serial.println(sy->getSample());
-  }
-  String pre_sine = "Number of SineSynths: ";
-  String pre_saw = " Number of SawtoothSynths: ";
-  Serial.println(pre_sine + s.getSineSynths().size() + pre_saw + s.getSawtoothSynths().size());
-  sleep(3);
+    //s.printTokens();
+    for(auto sy : s.getSineSynths()) {
+        //Serial.println(sy->getSample());
+    }
+  
+    sleep(3);
 }
 
 void printDebug() {
-  
+    String pre_sine = "Number of SineSynths: ";
+    String pre_saw = " Number of SawtoothSynths: ";
+    Serial.println(pre_sine + s.getSineSynths().size() + pre_saw + s.getSawtoothSynths().size());
 }
